0ebb4dc6bbd3c84dff7109664fe1471a454f5d3f
AmjadAjrami <AjiAjrmai@gmail.com>
2023-09-02 14:03:40 +0300

    Updating Helper Functions

--- resources/views/payment_webview.blade.php
+++ resources/views/payment_webview.blade.php
@@ -28,12 +28,15 @@
      $data = openssl_encrypt($input, "AES-128-CBC", $ky, 0, $iv);  return $data;
     }
 
+    $sadadId = \App\Models\Setting::query()->where('key', 'sadadId')->first()->value;
+    $setting_secretKey = \App\Models\Setting::query()->where('key', 'secretKey')->first()->value;
+
      $sadad_checksum_array = array();
      $sadad__checksum_data = array();
      $txnDate = date('Y-m-d H:i:s');
      $email ='example@example.com';
-     $secretKey = 'Tj3MjrCSsK4GrBZv';
-     $merchantID = '3163340';
+     $secretKey = $setting_secretKey;
+     $merchantID = $sadadId;
      $sadad_checksum_array['merchant_id'] = $merchantID;
      $sadad_checksum_array['ORDER_ID'] = $order_id;
      $sadad_checksum_array['WEBSITE'] = 'www.example.com';
@@ -87,16 +90,18 @@
 //            '</form>';
 @endphp
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300&display=swap" rel="stylesheet">
 
 <style>
-    .sadadpaynow{
+    .sadadpaynow {
         background: #cd6a77 !important;
         border: 1px #cd6a77 solid !important;
     }
+
     .logo {
         display: block;
         margin-left: auto;
@@ -105,60 +110,78 @@
         margin-bottom: 50px;
         margin-top: 50px;
     }
-    .logo img{
+
+    .logo img {
         width: 200px !important;
     }
-    html{
+
+    html {
         @if($lang === 'ARB')
-            direction: rtl;
+             direction: rtl;
         @else
-            direction: ltr;
-        @endif
+             direction: ltr;
+    @endif
+
     }
-    body{
+
+    body {
         float: left !important;
         margin-left: 30px !important;
         margin-right: 30px !important;
         font-size: 20px !important;
         font-family: 'Almarai', sans-serif;
     }
+
     input::placeholder {
         font: 1rem/3 'Almarai', sans-serif;
         padding-right: 10px !important;
     }
-    .card-js{
+
+    .card-js {
         margin-top: 20px !important;
     }
+
     .card-js input, .card-js select {
         font-size: 20px !important;
         font-weight: bold !important;
         height: 45px !important;
         font-family: 'Almarai', sans-serif;
     }
+
     input#the-card-name-id,
     input.expiry,
-    input.cvc{
+    input.cvc {
         padding-right: 10px !important;
     }
-    .card-js .icon{
+
+    .card-js .icon {
         top: 15px !important;
     }
-    label.sadadCardLbl{
+
+    label.sadadCardLbl {
         font-family: 'Almarai', sans-serif;
     }
-    button{
+
+    button {
         font-size: 20px !important;
         margin-top: 20px !important;
         font-weight: bold !important;
     }
-    input[type="checkbox"]{
+
+    input[type="checkbox"] {
         margin-left: 10px !important;
     }
 </style>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     var sadadGetChecksum = function () {
         // afterChecksumSubmit($('#sadad_div').html());
@@ -168,8 +191,10 @@
 <div class="logo">
     <img src="{{ asset('logo.png') }}">
 </div>
-<div id="sadad_cc_container" data-sd-lang="{{ $lang }}" data-i-color="#cd6a77" data-apple-enable="1" data-apple-sadadid="3163340" data-apple-bizname="Glamgo App Electronic" data-apple-domain="glamgoapp.com" data-cbfunc="sadadGetChecksum"></div>
+<div id="sadad_cc_container" data-sd-lang="{{ $lang }}" data-i-color="#cd6a77" data-apple-enable="1"
+     data-apple-sadadid="3163340" data-apple-bizname="Glamgo App Electronic" data-apple-domain="glamgoapp.com"
+     data-cbfunc="sadadGetChecksum"></div>
 <div id="sadad_div">
-    <?php echo '<form id="sadadFinalForm1">'. implode('', $sAry1) .'</form>'; ?>
+    <?php echo '<form id="sadadFinalForm1">' . implode('', $sAry1) . '</form>'; ?>
 </div>
 <script src="https://sadadqa.com/jslib/sadad1.js"></script>
